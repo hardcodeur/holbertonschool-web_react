70e430bf39f1e775462f5af3602c9e0aa563aa9b
bbel <bweb33@protonmail.com>
2024-12-13 16:19:02 +0100

    fix 1.1: exo 5

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,6 +1,6 @@
 import "./Notifications.css";
 import { getLatestNotification } from "./utils";
-import closeBtn from "./assets/close-button.png";
+import closeBtn from "./assets/close-icon.png";
 
 function Notifications() {
     return (
@@ -8,11 +8,11 @@ function Notifications() {
         <div className="notifications">
             <div className="notif-head">
                 <p>Here is the list of notifications</p>
-                <button onClick={console.log()} aria-label="close" style={{cursor:"pointer",border: "none",backgroundColor:"transparent"}}><img src={closeBtn} alt="close" /></button>
+                <button onClick={console.log("Close button has been clicked")} aria-label="close" style={{cursor:"pointer",border: "none",backgroundColor:"transparent"}}><img src={closeBtn} alt="close" /></button>
             </div>
             <ul>
-                <li data-priority >New course available</li>
-                <li data-priority >New resume available</li>
+                <li data-priority="default" >New course available</li>
+                <li data-priority="urgent" >New resume available</li>
                 <li dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
             </ul>
         </div>
