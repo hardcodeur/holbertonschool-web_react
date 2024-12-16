5b9f244e1637fd960f75f13d161f0d2aebe7f1d3
bbel <bweb33@protonmail.com>
2024-12-16 15:31:39 +0100

    fix : exo6

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -2,13 +2,17 @@ import "./Notifications.css";
 import { getLatestNotification } from "./utils";
 import closeBtn from "./assets/close-icon.png";
 
+function btnClose(){
+    console.log('Close button has been clicked');
+}
+
 function Notifications() {
     return (
         <>
         <div className="notifications">
             <div className="notif-head">
                 <p>Here is the list of notifications</p>
-                <button onClick={console.log("Close button has been clicked")} aria-label="close" style={{textAlign: "end",cursor:"pointer",border: "none",backgroundColor:"transparent"}}><img src={closeBtn} alt="close" /></button>
+                <button onClick={btnClose} aria-label="close" style={{textAlign: "end",cursor:"pointer",border: "none",backgroundColor:"transparent"}}><img src={closeBtn} alt="close" /></button>
             </div>
             <ul>
                 <li data-priority="default" >New course available</li>
