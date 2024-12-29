36a86c27901554a9b77429517a1eec049c28d536
bbel <bweb33@protonmail.com>
2024-12-29 16:04:12 +0100

    new : exo3

---
+++ react_props/task_3/dashboard/src/App/App.jsx
@@ -0,0 +1,31 @@
+
+import Notifications from "../Notifications/Notifications.jsx";
+import Header from "../Header/Header.jsx";
+import Login from "../Login/Login.jsx";
+import Footer from "../Footer/Footer.jsx";
+import { getLatestNotification } from "../utils/utils.js";
+import './App.css'
+
+const notificationsList = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+];
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications notifications = {notificationsList} />
+      </div>
+      <Header/>
+      <Login />
+      <Footer />
+    </>
+  )
+}
+
+export default App
+
+
+
